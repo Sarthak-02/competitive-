fba77ea5afe63e5ae2c964024b5bd38f3718ebfa
Sarthak Gupta <sarthakg555@gmail.com>
2017-04-04 23:57:01 +0530

    Update and rename HOG to HOG.py

--- HOG.py
+++ HOG.py
@@ -7,3 +7,12 @@ from skimage import data, color, exposure
 img = cv2.imread("path/of/the/file")
 image = color.rgb2gray(img)
 fd, hog_image = hog(image, orientations=8, pixels_per_cell=(8, 8),cells_per_block=(1, 1), visualise=True)
+
+
+**************************************************************************************************************************
+
+using openCV
+
+img = cv2.imread("path/of/the/file")
+hog = cv2.HOGDescriptor()
+h = hog.compute(img)
